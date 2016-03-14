7b6c8d5efebb6a6b6ac7cd03139582787fbd781e
Sebastian Krysmanski <noreply@manski.net>
2016-03-14 14:41:51 +0100

    Initial checkin

---
+++ TestProj/TestClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TestProj
+{
+	class TestClass
+	{
+		public void Func()
+		{
+			var list = new List<int>();
+
+			list.Add(5);
+			list.Add(6);
+		}
+	}
+}
